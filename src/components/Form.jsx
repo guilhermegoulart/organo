a04be6eddbd667e402628c276746c94907341a59
Guilherme Goulart <guilhermegoulartm@outlook.com>
2023-04-12 00:21:03 -0300

    DropdownList, using props, map and key parameter; Put the new componet in the form, creating a array of strings (itens); Stylizing with tailwind css framework, and... Using the map method to create the itens, with arrow funtion.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,17 @@
+import { DropDownList } from "./DropdownList";
 import { TextField } from "./TextField";
 
 export function Form(props) {
+  const teams = [
+    "Programação",
+    "Front-End",
+    "DataScience",
+    "DevOps",
+    "UX e Design",
+    "Mobile",
+    "Inovacão e Gestão",
+  ];
+
   return (
     <section className="flex justify-center ">
       <form className="my-20 mx-0 bg-cinza rounded-2xl py-4 px-16 shadow-2 ">
@@ -10,6 +21,7 @@ export function Form(props) {
         <TextField label="Nome" placeholder="Digite seu nome" />
         <TextField label="Cargo" placeholder="Digite seu cargo" />
         <TextField label="Imagem" placeholder="Informe o endereço da imagem" />
+        <DropDownList label="Time" itens={teams} />
       </form>
     </section>
   );
